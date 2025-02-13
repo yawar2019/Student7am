87028f0e826fab4f05a07e1f856139e6d2ce019a
Yawar2019 <yawarali17@gmail.com>
2025-02-13 08:57:51 +0530

    updated routes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,32 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+
+  user={
+    id:"",
+    name:""
+  }
+constructor(private route:ActivatedRoute)
+{
+ 
+}
+  ngOnInit(): void {
+  
+    // this.user={
+    //   id:this.route.snapshot.params['id'],
+    //   name:this.route.snapshot.params['name']
+    // }
+
+    this.route.params.subscribe((data:Params)=>{
+      this.user={
+        id:data['id'],
+        name:data['name']
+      }
+    })
+
+
+  }
+
 
 }
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet,HomeComponent,AboutComponent,ContactComponent,RouterLink],
+  imports: [RouterOutlet,HomeComponent,AboutComponent,ContactComponent,RouterLink,RouterLinkActive],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
