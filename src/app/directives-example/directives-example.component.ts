457abf3d757b533aaabff41a9d40f283a7aa4c3a
Yawar2019 <yawarali17@gmail.com>
2025-02-21 09:11:52 +0530

    added Directives

--- src/app/directives-example/directives-example.component.ts
+++ src/app/directives-example/directives-example.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './directives-example.component.css'
 })
 export class DirectivesExampleComponent {
-isAvailable=true;
+isAvailable:unknown;
+hero='help';
+Cars=['BMW','THAR','TESLA','TATA'];
+Sports=[
+  {id:11,name:'cricket'},
+  {id:12,name:'kabadi'},
+  {id:13,name:'hockey'},
+  {id:14,name:'chess'}
+]
+
+firstObj={
+  first:true,
+  second:false
+  //third:false
+}
+firstStyle={"color":"red","background-color":"gray"};
 }
